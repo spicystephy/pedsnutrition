99d7905305cdfc17cc309f6296d413a8b0d0a6fd
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-10-09 16:58:05 -0700

    added page links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,85 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
 // import "./style.css";
 
 export default function Header() {
-    return (
-        <div>
-            HEADER Navbar that includes Peds Nutrition, LLC left; pages centered; fb and insta icons; Contact Us btn
-        </div>
-    )
+  return (
+    <nav className="navbar">
+      <div>
+        <Link
+          to="/"
+          className={
+            window.location.pathname === "/"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Peds Nutrition, LLC.</p>
+        </Link>
+        <Link
+          to="/About"
+          className={
+            window.location.pathname === "/About"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>About Us</p>
+        </Link>
+        <Link
+          to="/Philosophy"
+          className={
+            window.location.pathname === "/Philosophy"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Our Philosophy</p>
+        </Link>
+        <Link
+          to="/Services"
+          className={
+            window.location.pathname === "/Services"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Our Services</p>
+        </Link>
+        <Link
+          to="/Resources"
+          className={
+            window.location.pathname === "/Resources"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Resources</p>
+        </Link>
+        <Link
+          to="/Referrals"
+          className={
+            window.location.pathname === "/Referrals"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Referrals</p>
+        </Link>
+        <Link
+          to="/Contact"
+          className={
+            window.location.pathname === "/Contact"
+              ? "navbar-item is-active"
+              : "navbar-item"
+          }
+        >
+          <p>Contact Us</p>
+        </Link>
+      </div>
+
+      {/* Navbar that includes Peds Nutrition, LLC left; pages centered; fb and insta icons; Contact Us btn */}
+    </nav>
+  );
 }
