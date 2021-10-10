8f7502130395611cef3778334d9ca55ec8741d78
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-10-09 17:15:35 -0700

    Cleaned footer, added links to phone and email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <h1>FOOTER Let's Talk!</h1>
-      <h2>Contact us today to get started, Contact Us btn</h2>
+      <h1>Let's Talk!</h1>
+      <h2>Contact us today to get started</h2>
+      {/* <h2>referral btn</h2> */}
       <div>
         <p>Location: Tucson, Az</p>
-        <p>Contact: (520)777-6784 caitlin@pedsnutrition.com</p>
+        <p>Contact:</p>
+        <a href="tel:520-777-6784">(520)777-6784</a> <a href="mailto:caitlin@pedsnutrition.com">caitlin@pedsnutrition.com</a>
         <p>Follow: Insta Link, FB Link</p>
       </div>
     </div>
