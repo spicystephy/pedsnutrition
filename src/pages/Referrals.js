f5ddf78d9db411821f3e891d7455c9d299b2cda8
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-10-09 17:14:56 -0700

    Cleaned referral page for dep

--- src/pages/Referrals.js
+++ src/pages/Referrals.js
@@ -4,8 +4,9 @@ function Referrals() {
     return (
         <div>
             <h1>Referrals</h1>
-            <p>Fill out the following. We will contact the client within 2 business days to schedule an appointment</p>
-            <h2>Insert form here: Child's Name* (First Name and Last Name), Date of Birth*(MM,DD,YYYY), Phone*((###)### ####), Reason for Referral*, Your Name and Contact Information* (Your preferred e-mail or phone number)Send Btn</h2>
+            <h2>Referal Form Coming Soon</h2>
+            {/* <p>Fill out the following. We will contact the client within 2 business days to schedule an appointment</p>
+            <h2>Insert form here: Child's Name* (First Name and Last Name), Date of Birth*(MM,DD,YYYY), Phone*((###)### ####), Reason for Referral*, Your Name and Contact Information* (Your preferred e-mail or phone number)Send Btn</h2> */}
         </div>
     )
 }
