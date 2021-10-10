00370679277a2ef89181311c11dedf3133bf59fd
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-10-09 17:14:30 -0700

    Cleaned contact us page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,8 @@ function Contact() {
             <h1>Contact Us</h1>
             <h2>Peds Nutrition, LLC</h2>
             <h3>Tucson AZ (520)777-6784 caitlin@pedsnutrition.com</h3>
-            <p>Insert form here: Your Name* (First Name and Last Name), Phone*((###)### ####), Email, Primary Concern*, Submit Btn</p>
+            {/* <p>Insert form here: Your Name* (First Name and Last Name), Phone*((###)### ####), Email, Primary Concern*, Submit Btn</p> */}
+            <h2>Form Coming Soon</h2>
         </div>
     )
 }
