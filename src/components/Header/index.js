2750669631df166f68eb6e67e3509723d742d44e
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-10-28 13:05:19 -0700

    reorganized navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,78 +6,93 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav className="navbar">
-      <div>
-        <Link
-          to="/"
-          className={
-            window.location.pathname === "/"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Peds Nutrition, LLC.</p>
-        </Link>
-        <Link
-          to="/About"
-          className={
-            window.location.pathname === "/About"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>About Us</p>
-        </Link>
-        <Link
-          to="/Philosophy"
-          className={
-            window.location.pathname === "/Philosophy"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Our Philosophy</p>
-        </Link>
-        <Link
-          to="/Services"
-          className={
-            window.location.pathname === "/Services"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Our Services</p>
-        </Link>
-        <Link
-          to="/Resources"
-          className={
-            window.location.pathname === "/Resources"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Resources</p>
-        </Link>
-        <Link
-          to="/Referrals"
-          className={
-            window.location.pathname === "/Referrals"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Referrals</p>
-        </Link>
-        <Link
-          to="/Contact"
-          className={
-            window.location.pathname === "/Contact"
-              ? "navbar-item is-active"
-              : "navbar-item"
-          }
-        >
-          <p>Contact Us</p>
-        </Link>
-      </div>
+      <Link
+        to="/"
+        className={
+          window.location.pathname === "/"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Peds Nutrition, LLC.</p>
+      </Link>
+
+      <Link
+        to="/About"
+        className={
+          window.location.pathname === "/About"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>About Us</p>
+      </Link>
+
+      <Link
+        to="/Philosophy"
+        className={
+          window.location.pathname === "/Philosophy"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Our Philosophy</p>
+      </Link>
+
+      <Link
+        to="/Services"
+        className={
+          window.location.pathname === "/Services"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Our Services</p>
+      </Link>
+
+      <Link
+        to="/Resources"
+        className={
+          window.location.pathname === "/Resources"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Resources</p>
+      </Link>
+
+      <Link
+        to="/Referrals"
+        className={
+          window.location.pathname === "/Referrals"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Referrals</p>
+      </Link>
+
+      <Link
+        to="/faqs"
+        className={
+          window.location.pathname === "/FAQs"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>FAQ</p>
+      </Link>
+
+      <Link
+        to="/Contact"
+        className={
+          window.location.pathname === "/Contact"
+            ? "navbar-item is-active"
+            : "navbar-item"
+        }
+      >
+        <p>Contact Us</p>
+      </Link>
 
       {/* Navbar that includes Peds Nutrition, LLC left; pages centered; fb and insta icons; Contact Us btn */}
     </nav>
