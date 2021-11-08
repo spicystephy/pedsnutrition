f14149f74378837d67f07a8a6b0245f54cd25aa5
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-11-08 12:51:06 -0700

    Changed home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,55 @@
 import React from "react";
+import { CardGroup } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
 function Home() {
   return (
     <div>
-      <h1>
-        Getting you and your child from where you are to where you want to be
-      </h1>
-      <h3>Contact Us Today(Link to Contact Us)</h3>
-      <h2>
-        We want to give your child a jump start to their healthy ever after
-      </h2>
-      <h2>What We Do</h2>
-      <h3>Personalized Nutrition</h3>
-      <p>
-        Our dietitian will work with you to help you meet your child’s
-        nutritional goals.
-      </p>
-      <h3>Infants</h3>
-      <p>
-        We can help with growth issues, fortification, and introducing solids.
-      </p>
-      <h3>Toddler</h3>
-      <p>
-        We want to eliminate meal time battles and put an end to picky eating.
-      </p>
-      <h3>All Children</h3>
-      <p>
-        We want to help any child with growth issues, food allergies, GI issues,
-        and more.
-      </p>
+      
+      <Card className="text-center">
+        <Card.Title>What We Do</Card.Title>
+        <Card.Header>
+          Our dietitian will work with you to help you meet your child’s
+          nutritional goals.
+        </Card.Header>
+        <Card.Body>
+          We want to give your child a jump start to their healthy ever after
+          <Card.Text>Personalized Nutrition</Card.Text>
+          <p>
+            Getting you and your child from where you are to where you want to
+            be
+          </p>
+        </Card.Body>
+      </Card>
+      <CardGroup className="text-center">
+      <Card  style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Infants </Card.Title>
+          <Card.Text>
+            We can help with growth issues, fortification, and introducing
+            solids.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Toddlers</Card.Title>
+          <Card.Text>
+            We want to eliminate meal time battles and put an end to picky
+            eating.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>All Children</Card.Title>
+          <Card.Text>
+            We want to help any child with growth issues, food allergies, GI
+            issues, and more.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </CardGroup>
     </div>
   );
 }
